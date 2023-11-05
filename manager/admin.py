a3ece16dd4aa97a569ac6bedf49644cd80d94547
Hlova Oleh <hlovaoleh0@gmail.com>
2023-11-05 12:50:41 +0200

    created project models with the following tables: TaskType; Position; Worker(AbstractUser); Task. Also implemented django admin panel

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from manager.models import TaskType, Task, Position, Worker
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("position",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("position",)}),)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,46 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = "Worker"
+        verbose_name_plural = "Workers"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ("U", "Urgent"),
+        ("H", "High"),
+        ("M", "Medium"),
+        ("L", "Low")
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default="M")
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")

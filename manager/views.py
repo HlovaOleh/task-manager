84b6db6dc5590407b4c2295a031304a8a2df4216
Hlova Oleh <hlovaoleh0@gmail.com>
2023-11-06 17:23:04 +0200

    added main page and navigation sidebar menu

--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import TaskType, Task, Worker, Position
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    task_type_count = TaskType.objects.count()
+    task_count = Task.objects.count()
+    workers_count = Worker.objects.count()
+    position_count = Position.objects.count()
+    context = {
+        "task_type_count": task_type_count,
+        "task_count": task_count,
+        "workers_count": workers_count,
+        "position_count": position_count
+    }
+    return render(request, "manager/index.html", context=context)
